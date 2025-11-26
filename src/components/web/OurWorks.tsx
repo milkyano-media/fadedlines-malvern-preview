6b1d831c3ed1150a5401c9276bc9560a257625f9
Fauzan Zaman <fauzanzaman000@gmail.com>
2025-11-26 16:55:04 +0700

    Change gallery except by Josh

--- src/components/web/OurWorks.tsx
+++ src/components/web/OurWorks.tsx
@@ -19,44 +19,8 @@ const CarauselGallery = () => {
                 alt: "High Skin Fade by Josh",
             },
             {
-                url: "https://s3.milkyano.com/milkyano/fadedlines-oakleigh/gallery/3.png",
-                alt: "High Skin Fade by Emman",
-            },
-            {
-                url: "https://s3.milkyano.com/milkyano/fadedlines-oakleigh/gallery/4.png",
-                alt: "Low Taper by Emman",
-            },
-            {
-                url: "https://s3.milkyano.com/milkyano/fadedlines-oakleigh/gallery/5.png",
-                alt: "Mid to High Burst Fade by Emman",
-            },
-            {
-                url: "https://s3.milkyano.com/milkyano/fadedlines-oakleigh/gallery/6.png",
-                alt: "High Drop Fade by Emman",
-            },
-            {
                 url: "https://s3.milkyano.com/milkyano/fadedlines-oakleigh/gallery/7.png",
                 alt: "Mid to High by Josh",
-            },
-            {
-                url: "https://s3.milkyano.com/milkyano/fadedlines-oakleigh/gallery/8.png",
-                alt: "High Skin Fade by Christos",
-            },
-            {
-                url: "https://s3.milkyano.com/milkyano/fadedlines-oakleigh/gallery/9.png",
-                alt: "Burst Fade by Emman",
-            },
-            {
-                url: "https://s3.milkyano.com/milkyano/fadedlines-oakleigh/gallery/10.png",
-                alt: "High Skin Fade by Christos",
-            },
-            {
-                url: "https://s3.milkyano.com/milkyano/fadedlines-oakleigh/gallery/11.png",
-                alt: "High Taper Fade by Christos",
-            },
-            {
-                url: "https://s3.milkyano.com/milkyano/fadedlines-oakleigh/gallery/12.png",
-                alt: "Low Drop Fade by Christos",
             },
         ])
     );
